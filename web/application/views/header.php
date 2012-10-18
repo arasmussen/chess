17db9f0b2aab5a29005f8b21e492cd89ce8cb7a0
Andrew <arasmussen@katworks.com>
2012-10-18 19:40:10 -0400

    added some styles and login in header

--- web/application/views/header.php
+++ web/application/views/header.php
@@ -3,18 +3,38 @@
   <head>
     <title>Chess AI</title>
     <link rel="stylesheet" type="text/css" href="/css/reset.css" />
+    <link rel="stylesheet" type="text/css" href="/css/styles.css" />
     <script type="text/javascript" src="/js/jquery-1.7.2.min.js"></script>
+    <script type="text/javascript" src="/js/scripts.js"></script>
     <meta http-equiv="content-type" content="text/html;charset=utf-8">
   </head>
   <body>
     <div id="center">
       <div id="header">
-        <?php
-          if ($is_logged_in) {
-            echo 'Logged in as: ' . $username . "\n";
-          } else {
-            echo 'Not logged in.' . "\n";
-          }
-        ?>
+        <div id="logo">
+        </div>
+        <?php if ($is_logged_in) { ?>
+          <div id="profileLink">
+            <a href="<?php echo "/profile/$username"; ?>">
+              <?php echo $username; ?>
+            </a>
+          </div>
+        <?php } else { ?>
+          <div id="login">
+            <form method="post" action="javascript:void(0)">
+              <div id="loginEmail">
+                <label>Email</label>
+                <input type="text" name="email">
+              </div>
+              <div id="loginPassword">
+                <label>Password</label>
+                <input type="password" name="password">
+              </div>
+              <div id="loginButton">
+                <input type="submit" value="Sign in">
+              </div>
+            </form>
+          </div>
+        <?php } ?>
       </div>
       <div id="page">
